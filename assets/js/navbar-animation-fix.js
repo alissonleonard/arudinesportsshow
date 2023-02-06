b15548cc0c1d980327ff85a0c9a1b3d34a289ea2
alissonleonard <alissonsouza123456@gmail.com>
2023-02-06 16:36:30 -0300

    navbar-animation-fix feito

---
+++ assets/js/navbar-animation-fix.js
@@ -0,0 +1,11 @@
+$('#navbarNav').on('show.bs.collapse' , function() {
+
+$('.auroratuning-banner').css('transform', 'translate(-50%,10%')
+
+});
+
+$('#navbarNav').on('hide.bs.collapse' , function() {
+
+    $('.auroratuning-banner').css('transform', 'translate(-50%, -50%')
+    
+    });
